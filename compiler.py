c87fbec7903f99bfb6c7f0452de0d3061c7065f2
Lime <lime.syh@gmail.com>
2018-02-24 10:41:54 +0800

    format coding style

--- compiler.py
+++ compiler.py
@@ -7,7 +7,7 @@
 
 '''
 Compiler for C language with python
-    
+
 Usage: python compiler.py -s [file] [options]
 
 Options:
@@ -16,27 +16,54 @@ Options:
     -l              lexer
     -p              parser
     -a              assembler, the assembler file is in the same path with compiler.py
-    
+
 Examples:
     python compiler.py -h
     python compiler.py -s source.c -a
 
 Enjoy ^_^.
 '''
+
 import re
 import sys
 import getopt
 
 # token比较大的分类
-TOKEN_STYLE = ['KEY_WORD', 'IDENTIFIER', 'DIGIT_CONSTANT',
-               'OPERATOR', 'SEPARATOR', 'STRING_CONSTANT']
+TOKEN_STYLE = [
+    'KEY_WORD', 'IDENTIFIER', 'DIGIT_CONSTANT',
+    'OPERATOR', 'SEPARATOR', 'STRING_CONSTANT'
+]
+
 # 将关键字、运算符、分隔符进行具体化
 DETAIL_TOKEN_STYLE = {
-    'include': 'INCLUDE', 'int': 'INT', 'float': 'FLOAT', 'char': 'CHAR', 'double': 'DOUBLE', 'for': 'FOR', 'if': 'IF', 'else': 'ELSE',
-    'while': 'WHILE', 'do': 'DO', 'return': 'RETURN', '=': 'ASSIGN', '&': 'ADDRESS',
-    '<': 'LT', '>': 'GT', '++': 'SELF_PLUS', '--': 'SELF_MINUS', '+': 'PLUS', '-': 'MINUS', '*': 'MUL', '/': 'DIV', '>=': 'GET', '<=': 'LET', '(': 'LL_BRACKET',
-    ')': 'RL_BRACKET', '{': 'LB_BRACKET', '}': 'RB_BRACKET', '[': 'LM_BRACKET', ']': 'RM_BRACKET', ',': 'COMMA', '\"': 'DOUBLE_QUOTE',
+    'include': 'INCLUDE',
+    'int': 'INT',
+    'float': 'FLOAT',
+    'char': 'CHAR',
+    'double': 'DOUBLE',
+    'for': 'FOR',
+    'if': 'IF',
+    'else': 'ELSE',
+    'while': 'WHILE',
+    'do': 'DO',
+    'return': 'RETURN',
+    '=': 'ASSIGN',
+    '&': 'ADDRESS',
+    '<': 'LT',
+    '>': 'GT',
+    '++': 'SELF_PLUS',
+    '--': 'SELF_MINUS',
+    '+': 'PLUS',
+    '-': 'MINUS',
+    '*': 'MUL',
+    '/': 'DIV',
+    '>=': 'GET',
+    '<=': 'LET',
+    '(': 'LL_BRACKET',
+    ')': 'RL_BRACKET',
+    '{': 'LB_BRACKET', '}': 'RB_BRACKET', '[': 'LM_BRACKET', ']': 'RM_BRACKET', ',': 'COMMA', '\"': 'DOUBLE_QUOTE',
     ';': 'SEMICOLON', '#': 'SHARP'}
+
 # 关键字
 keywords = [['int', 'float', 'double', 'char', 'void'],
            ['if', 'for', 'while', 'do', 'else'], ['include', 'return']]
--- source.c
+++ source.c
@@ -1,28 +1,32 @@
 #include <stdio.h>
-int main(){
-    int score[ 6 ] = { 76, 82, 90, 86, 79, 62 } ;
-    int credit[ 6 ] = { 2, 2, 1, 2, 2, 3 } ;
-    int stu_number ;
-    float mean, sum ;
-    int temp ;
-    int i ;
 
-    printf( "please input your student number:" ) ;
-    scanf( "%d" , &stu_number ) ;
-    sum = 0 ;
-    temp = 0 ;
-    for( i = 0 ; i < 6 ; i++ ){
-         sum = sum + score[i] * credit[i] ;
-         temp = temp + credit[i] ;
+int main() {
+    int score[6] = {76, 82, 90, 86, 79, 62};
+    int credit[6] = {2, 2, 1, 2, 2, 3};
+    int stu_number;
+    float mean, sum;
+    int temp;
+    int i;
+
+    printf("please input your student number:");
+    scanf("%d" , &stu_number);
+
+    sum = 0;
+    temp = 0;
+
+    for(i = 0 ; i < 6 ; i++) {
+         sum = sum + score[i] * credit[i];
+         temp = temp + credit[i];
     }
+
     mean = sum / temp ;
-    if( mean >= 60 ){
+    if(mean >= 60) {
          mean = mean - 60 ;
-         printf( "the score of student number %d is %f higher than 60.\n", stu_number, mean ) ;
-    }
-    else{
+         printf("the score of student number %d is %f higher than 60.\n", stu_number, mean);
+    } else {
          mean = 60 - mean ;
          printf( "the score of student number %d is %f lower than 60.\n", stu_number, mean ) ;
     }
-    return 0 ;
+
+    return 0;
 }
